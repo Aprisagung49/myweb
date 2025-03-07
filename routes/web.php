dc8ae9bdfd59726a60b983539175f298004e2a16
Your Name <you@example.com>
2025-03-07 16:32:44 +0700

    all

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,37 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+class News
+{
+    public static function all()
+    {
+        return [
+            [
+            'id' => 1,
+            'slug' => 'programing-zaman-now',
+            'title' => 'PROGRAMING ZAMAN NOW',
+            'author' => 'Apris Agung Wiresa',
+            'body' =>   'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.'
+            ],
+            [
+            'id' => 2,
+            'slug' => 'learn-css-tailwind',
+            'title' => 'LEARN CSS TAILWIND',
+            'author' => 'IMAN',
+            'body' =>   'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.'
+            ],
+            [
+            'id' => 3,
+            'slug' => 'learn-laravel-php',
+            'title' => 'LEARN LARAVEL PHP',
+            'author' => 'GAMA',
+            'body' =>   'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.'
+            ]   
+        ];
+    }
+}
+
+
 Route::get('/', function () {
     return view('home', ['title' => 'Halaman Awal']);
 });
@@ -48,28 +79,5 @@ Route::get('/news/{slug}', function($slug){
 });
 
 Route::get('/news', function () {
-    return view('news', ['title' => 'Halaman Beritaku', 'news' => 
-    [
-        [
-        'id' => 1,
-        'slug' => 'programing-zaman-now',
-        'title' => 'PROGRAMING ZAMAN NOW',
-        'author' => 'Apris Agung Wiresa',
-        'body' =>   'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.'
-        ],
-        [
-        'id' => 2,
-        'slug' => 'learn-css-tailwind',
-        'title' => 'LEARN CSS TAILWIND',
-        'author' => 'IMAN',
-        'body' =>   'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.'
-        ],
-        [
-        'id' => 3,
-        'slug' => 'learn-laravel-php',
-        'title' => 'LEARN LARAVEL PHP',
-        'author' => 'GAMA',
-        'body' =>   'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.'
-        ]   
-    ]]);
+    return view('news', ['title' => 'Halaman Beritaku', 'news' => News::all()]);
 });
